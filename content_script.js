bef34d434757c79759de3029cbf5d445af550697
rvion <vion.remi@gmail.com>
2014-09-09 23:42:14 +0200

    add clickable links in the popup

--- content_script.js
+++ content_script.js
@@ -1,6 +1,12 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action == "getDOM") {
+    var l = document.links;
+    LINKS = []
+    for (var i = 0; i < l.length; i++) {
+      LINKS.push({ "title": l[i].text, "url": l[i].href });
+    }
     var domInfo = {
+      links: LINKS,
       total: document.querySelectorAll('*').length,
       inputs: document.querySelectorAll('input').length,
       buttons: document.querySelectorAll('button').length
--- popup.js
+++ popup.js
@@ -1,7 +1,29 @@
+function onAnchorClick(event) {
+  chrome.tabs.create({
+    selected: true,
+    url: event.srcElement.href
+  });
+  return false;
+}
+function buildPopupDom(divName, data) {
+  var popupDiv = document.getElementById(divName);
+  var ul = document.createElement('ul');
+  popupDiv.appendChild(ul);
+  for (var i = 0, ie = data.length; i < ie; ++i) {
+    var a = document.createElement('a');
+    a.href = data[i].url;
+    a.appendChild(document.createTextNode(data[i].title));
+    a.addEventListener('click', onAnchorClick);
+    var li = document.createElement('li');
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+}
 function setDOMInfo(info) {
     document.getElementById('total').textContent   = info.total;
     document.getElementById('inputs').textContent  = info.inputs;
     document.getElementById('buttons').textContent = info.buttons;
+    buildPopupDom("typedUrl_div",info.links)
     console.log(info)
 }
 chrome.tabs.getSelected(null, function(tab) {
