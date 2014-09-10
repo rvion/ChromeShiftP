4657c3f4ac8d3fcc2b3f0941ff73964dec083d03
rvion <vion.remi@gmail.com>
2014-09-10 09:35:42 +0200

    add jquery and select2

--- popup.js
+++ popup.js
@@ -6,28 +6,30 @@ function onAnchorClick(event) {
   return false;
 }
 function buildPopupDom(divName, data) {
-  var popupDiv = document.getElementById(divName);
-  var ul = document.createElement('ul');
-  popupDiv.appendChild(ul);
+  var popupDiv = document.getElementById("query");
   for (var i = 0, ie = data.length; i < ie; ++i) {
-    var a = document.createElement('a');
-    a.href = data[i].url;
+    var a = document.createElement('option');
+    a.value = data[i].url;
     a.appendChild(document.createTextNode(data[i].title));
     a.addEventListener('click', onAnchorClick);
-    var li = document.createElement('li');
-    li.appendChild(a);
-    ul.appendChild(li);
+    popupDiv.appendChild(a);
   }
 }
 function setDOMInfo(info) {
-    document.getElementById('total').textContent   = info.total;
-    document.getElementById('inputs').textContent  = info.inputs;
-    document.getElementById('buttons').textContent = info.buttons;
-    buildPopupDom("typedUrl_div",info.links)
-    console.log(info)
+  //console.log(info);
+  document.getElementById('total').textContent   = info.total;
+  document.getElementById('inputs').textContent  = info.inputs;
+  document.getElementById('buttons').textContent = info.buttons;
+  buildPopupDom("typedUrl_div",info.links)
+  //console.log(info)
 }
 chrome.tabs.getSelected(null, function(tab) {
   chrome.tabs.sendMessage(tab.id, {action: "getDOM"}, function(response) {
+    //console.log(response);
     setDOMInfo(response)
   });
 });
+
+$(function(){
+  $("select#query").select2()
+})
